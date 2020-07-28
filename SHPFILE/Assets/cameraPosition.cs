11546e941d3faece45fd24e0238251d2e732b9d6
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-07-28 15:01:02 +1000

    Immersive tool for Archeology

---
+++ SHPFILE/Assets/cameraPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraPosition : MonoBehaviour
+{
+   private Camera cam;
+
+    void Start()
+    {
+            cam = Camera.main;    
+            cam.rect = new Rect(575584f, 575584f, 7.127485f,20f);
+    }
+}
+
+
